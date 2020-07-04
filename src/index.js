5caf3bfe165d0b5fb48410094ea432c29d3bb085
yayxs <leaderywl@163.com>
2020-07-04 17:18:34 +0800

    add vue2x code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-30 22:53:49
+ * @LastEditTime: 2020-06-30 23:07:31
+ * @LastEditors: Please set LastEditors
+ * @Description: your vue
+ * @FilePath: \vue-source-code\src\index.js
+ */
