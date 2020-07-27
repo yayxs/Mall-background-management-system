84a2e97992c412a3e41d1cc39c583d6aaf96aafa
yayxs <leaderywl@163.com>
2020-07-27 23:27:51 +0800

    createEle

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-27 22:05:34
- * @LastEditTime: 2020-07-27 22:06:13
+ * @LastEditTime: 2020-07-27 22:09:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-source-code\README.md
 -->
 
-- 衍生出的面试题
+- [衍生出的面试题](https://github.com/yayxs/vue-source-code/blob/master/docs/%E8%A1%8D%E7%94%9F%E5%87%BA%E7%9A%84%E9%9D%A2%E8%AF%95%E9%A2%98.md)
--- examples/vue-cli2-app/src/App.vue
+++ examples/vue-cli2-app/src/App.vue
@@ -1,35 +1,19 @@
-<!--
- * @Author: your name
- * @Date: 2020-07-27 21:53:20
- * @LastEditTime: 2020-07-27 22:04:26
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \vue-source-code\examples\vue-cli2-app\src\App.vue
--->
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <router-view />
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  mounted() {
-    console.log(this.msg);
-  },
-  data() {
-    return {
-      msg: '',
-    };
-  },
-};
+  name: 'App'
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
