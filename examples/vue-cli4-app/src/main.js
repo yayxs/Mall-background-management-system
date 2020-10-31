30a9140fd798be17ed8ff3e82022ffabcedeb9d6
yayxs <leaderywl@163.com>
2020-10-31 10:37:17 +0800

    docs:vue-docs分支创建

--- examples/vue-cli4-app/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import TodoList from './components/TodoList.vue'
-Vue.config.productionTip = false
-Vue.component('todo-list',TodoList)
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
