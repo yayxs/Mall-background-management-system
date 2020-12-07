17797bedf32efdc0be9b42ce7d70cad3cfee42b7
yangwl <yangwl@zonekey.com.cn>
2020-12-07 18:21:53 +0800

    feat:添加vue2-app

---
+++ examples/vue2-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="box" v-for="item in list" :ref="setItemRef" :key="item">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      itemRefs: [],
+      list: [1, 2, 3],
+    };
+  },
+  methods: {
+    setItemRef(el) {
+      console.log(el)
+      this.itemRefs.push(el);
+    },
+  },
+  beforeUpdate() {
+    this.itemRefs = [];
+  },
+  updated() {
+    console.log(this.itemRefs);
+  },
+};
+</script>
+
+<style></style>
