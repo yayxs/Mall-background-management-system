3932d3f1feb75f0048856a2df08441052b8d394d
yangwl <yangwl@zonekey.com.cn>
2020-12-14 16:29:01 +0800

    feat:add composition api notes

---
+++ examples/composition-api-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <CounterApp />
+  <!-- <WatchEffect /> -->
+</template>
+
+<script>
+// import List from './components/List.vue'
+import CounterApp from './components/Counter.vue';
+// import WatchEffect from './components/WatchEffect.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    CounterApp,
+    // WatchEffect
+  }
+}
+</script>
---
+++ examples/composition-api-app/src/components/WatchEffect.vue
@@ -0,0 +1,16 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+    setup(){
+        // watchEffect(()=>{
+        //     document.body.innerHTML = `count is ${state.count}`
+        // })
+    }
+}
+</script>
+<style scoped>
+
+</style>
